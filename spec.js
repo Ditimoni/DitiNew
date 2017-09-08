bd9606315859ac44c992526f19c80b94f323aaf0
Borpatra Gohain, Ditimoni <ditimoni.borpatragohain@ge.com>
2017-09-08 19:22:59 +0530

    Commited

--- spec.js
+++ spec.js
@@ -2,10 +2,16 @@ var helper = require('./helper');
 
 describe('Protractor demo', function()
 {
+    beforeEach(function(){
+        browser.get(browser.params.url);
+    });
+
+    afterEach(function(){
+        browser.manage().deleteAllCookies();
+    });
+
 it('should have a title', function()
     {
-       // browser.get('https://angularjs.org');
-        browser.get(browser.params.url);
        // browser.driver.get('https://cb62t.am.health.ge.com/cbwui');
         //browser.sleep(50000);
         var title = 'Etsy.com | Shop for anything from creative people everywhere';
@@ -13,7 +19,7 @@ it('should have a title', function()
         expect(browser.getTitle()).toEqual(title);
     });
 
-    it('registration to the site',function(){
+    it('error while sign in to the site',function(){
         var signinButton = element(by.id('sign-in'));
         var usernameField = element(by.id('username-existing'));
         var passwordField = element(by.id('password-existing'));
@@ -27,4 +33,17 @@ it('should have a title', function()
        // expect(errorUserName.getText()).toEqual('Username is invalid.');
         expect(errorUserName.getText()).toBe('Username is invalid.');
 });
+    it('Registration to the site', function(){
+        var registrationBtn = element(by.id('register'))
+        var firstName =element(by.id('first-name'))
+        var email = element(by.id('email'))
+        var password = element(by.id('password'))
+        var confirmPassword = element(by.id('password-repeat'))
+        registrationBtn.click()
+        helper.waitElementToBeVisible(firstName)
+        firstName.sendKeys('ditimoni')
+        email.sendKeys('diti@gmail.com')
+        password.sendKeys('password')
+        confirmPassword.sendKeys('password')
+    });
 });
